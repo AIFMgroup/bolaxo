cc306d88330da7e749da9d986c7e7a34514b2a43
Christopher Genberg <christopher.genberg@aifm.se>
2025-12-28 13:04:54 +0100

    chore: reduce build noise (conditional Sentry, prisma config)

--- next.config.js
+++ next.config.js
@@ -120,37 +120,27 @@ module.exports = withNextIntl(nextConfig)
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+const hasSentryAuthToken = !!process.env.SENTRY_AUTH_TOKEN
 
-    org: "billboardbee",
-    project: "javascript-nextjs-19",
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  }
-);
+// Only enable the Sentry webpack plugin (releases + sourcemaps) when a token is present.
+// This avoids noisy build warnings in environments where Sentry isn't fully configured yet.
+if (hasSentryAuthToken) {
+  module.exports = withSentryConfig(
+    module.exports,
+    {
+      // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+      org: "billboardbee",
+      project: "javascript-nextjs-19",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      telemetry: false,
+    },
+    {
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+      silent: !process.env.CI,
+      widenClientFileUpload: true,
+      tunnelRoute: "/monitoring",
+      disableLogger: true,
+      automaticVercelMonitors: true,
+    }
+  )
+}
---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+// Prisma config file (Prisma 6+) to avoid deprecated package.json#prisma config.
+// See: https://pris.ly/prisma-config
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+})
+
+
